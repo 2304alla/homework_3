336e73e5799dbb9ee0c287132d36d5d6f5abaae0
allakoval <2448572@gmail.com>
2022-10-31 20:24:06 +0300

    task19.21/23

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+
+// Напишите программу, 
+// которая принимает на вход пятизначное число
+//  и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+int Palindrome(int number)
+{
+    int number5 = number % 10;
+    int number1 = number / 10000;
+    int number2 = number / 1000 % 10;
+    int number4 = number / 10 % 10;
+
+
+    if (number1 == number5 && number2 == number4) return number;
+
+    return 0;
+
+
+
+}
+
+Console.Write("Ведите пятизначное  число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99999 || num < 10000) Console.WriteLine("Не верное значение");
+else
+{
+    int palindrome = Palindrome(num);
+    string result = palindrome == 0 ? "Не является палиндромом" : "Является палиндромом";
+
+    Console.WriteLine(result);
+}
---
+++ task23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 23
+
+// Напишите программу, 
+// которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+void Quarter(int number)
+{
+    int i = 1;
+    while (i <= number)
+    {
+        Console.WriteLine($"{i,4}   {i * i * i,4}");
+        i++;
+    }
+}
+
+
+Console.Write("Ведите натуральное число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+
+if (num > 0) Quarter(num);
+else Console.WriteLine("Введите число,которое больше 0");
