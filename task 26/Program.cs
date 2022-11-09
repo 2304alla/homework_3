4cfd33f4634ddbdb06ea9a863fd9fcbdb77041fb
allakoval <2448572@gmail.com>
2022-11-09 21:44:25 +0500

    2

--- task 26/Program.cs
+++ task 26/Program.cs
@@ -1,7 +1,25 @@
-﻿Задача 26: Напишите программу, которая принимает
-на вход число и выдаёт количество цифр в числе.
-456 -> 3
-78 -> 2
-89126 -> 5
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
 
 
+int CountDigit( int num)
+{
+    int count = 0;
+    while (num!=0)
+    {
+        count++;
+        num/=10;
+    }
+    return count;
+}
+
+
+
+Console.WriteLine("Введите число");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int countDigit = CountDigit(number);
+Console.WriteLine($"В числе {number} - {countDigit}  цифр  ");
---
+++ task30/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int FillArray(int[] len)
+{
+    int[] arr =new int[len];
+    Random rnd= new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rnd.Next(0,2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+    if (i<arr.Length-1) Console.Write(", ");
+
+    }
+    Console.Write("]");
+}
+Console.Write("Введите количесво элеметов массива ");
+int length= Convert.ToInt32(Console.ReadLine());
+int[] array = CreatArray(length);
+FillArray(array);
+PrintArray(array);
+
+
+
+
+
